92b7b3015107d6c63b016c6d137fe55cefd16394
Jordancoder13 <shivamtripathi74237@gmail.com>
2024-09-30 12:39:25 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,34 @@
-/**
- * @param {string} s
+ @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
     
+    let stack = [];
+    
+    let matchingBracket = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+   
+    for (let char of s) {
+       
+        if (matchingBracket[char]) {
+            
+            let topElement = stack.length ? stack.pop() : '#';
+            if (topElement !== matchingBracket[char]) {
+                return false;
+            }
+        } else {
+            
+            stack.push(char);
+        }
+    }
+
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
 
